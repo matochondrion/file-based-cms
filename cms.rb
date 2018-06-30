25235b02fdd52ccce31d6b363eb1276914a9fdcc
matochondrion <matochondrion@gmail.com>
2018-06-30 16:41:01 +0000

    lesson: Viewing Text Files, LS solution

--- cms.rb
+++ cms.rb
@@ -5,10 +5,20 @@ require 'tilt/erubis'
 
 root = File.expand_path('..', __FILE__)
 
-get '/' do
+before do
+
   @files= Dir.glob(root + '/data/*').map do |path|
     File.basename(path)
   end
+end
+
+get '/' do
   erb :index
 end
 
+get '/:filename' do
+  file_path = root + '/data/' + params[:filename]
+
+  headers['Content-Type'] = 'text/plain; charset=utf8'
+  File.read(file_path)
+end
