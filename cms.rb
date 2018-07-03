503caa384fbfa80bf7041faa3a359835c64ab7c8
matochondrion <matochondrion@gmail.com>
2018-07-03 02:55:11 +0000

    lesson: Deleting Documents, LS solution

--- cms.rb
+++ cms.rb
@@ -94,3 +94,13 @@ post '/:filename' do
   redirect '/'
 end
 
+post '/:filename/delete' do
+  file_path = File.join(data_path, params[:filename])
+
+  File.delete(file_path)
+
+  session[:message] = "#{params[:filename]} has been deleted."
+  redirect '/'
+end
+
+
