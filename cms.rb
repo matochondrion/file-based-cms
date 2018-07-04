d96bb7fcf52c383604578273a8c0a6b310678405
matochondrion <matochondrion@gmail.com>
2018-07-04 05:59:18 +0000

    lesson: Storing Hashed Passwords, LS solution

--- cms.rb
+++ cms.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'redcarpet'
 require 'yaml'
+require 'bcrypt'
 
 configure do
   enable :sessions
@@ -33,11 +34,14 @@ def load_user_credentials
   YAML.load_file(credentials_path)
 end
 
-def valid_user?(username, password)
-  load_user_credentials.any? do |valid_username, valid_password|
-    puts valid_username
-    puts valid_password
-    username == valid_username && password == valid_password
+def valid_credentials?(username, password)
+  credentials = load_user_credentials
+
+  if credentials.key?(username)
+    bcrypt_password = BCrypt::Password.new(credentials[username])
+    bcrypt_password == password
+  else
+    false
   end
 end
 
@@ -76,7 +80,7 @@ get '/users/signin' do
 end
 
 post '/users/signin' do
-  if valid_user?(params[:username], params[:password])
+  if valid_credentials?(params[:username], params[:password])
     session[:username] = params[:username]
     session[:message] = "Welcome!"
     redirect '/'
@@ -164,17 +168,3 @@ post '/:filename/delete' do
   redirect '/'
 end
 
-=begin
-* create a users.yml file
-* write a method to check if a pair of username and password are valid in the
-.yml fihle
-  - load a file with File.read
-  - pass it to YAML.load() and assign a new variable
-  - iterate over the hashe with key value pairs and check of there are any matches
-
-* if a name matches then sign user in
-* format of yaml file:
-  ----
-
-  ...
-=end
